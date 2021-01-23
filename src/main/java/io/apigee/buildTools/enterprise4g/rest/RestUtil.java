ba65393fc5822699aad1155f2c1fe46ec6280b70
Sai Saran Vaidyanathan <ssvaidyanathan@google.com>
2021-01-22 16:01:17 -0800

    Fix for #153

--- src/main/java/io/apigee/buildTools/enterprise4g/rest/RestUtil.java
+++ src/main/java/io/apigee/buildTools/enterprise4g/rest/RestUtil.java
@@ -945,7 +945,8 @@ public class RestUtil {
             logger.error(e.getMessage());
             throw new IOException(e.getMessage());
          }
-        
+    	//fix for Issue153
+    	headers.set("X-GOOG-API-FORMAT-VERSION", 2);
     	logger.info(PrintUtil.formatRequest(request));
         return request.execute();
     }
